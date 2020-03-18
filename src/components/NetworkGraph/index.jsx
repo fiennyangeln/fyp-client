f4ff8f9285f84889a903742cd51a270fd8beb8e2
Fienny Angelina <fifi.angelina@yahoo.com>
2020-03-18 16:19:26 +0800

    Add play animation

--- src/components/NetworkGraph/index.jsx
+++ src/components/NetworkGraph/index.jsx
@@ -3,7 +3,11 @@ import Tree from 'react-d3-tree';
 import Slider from '@material-ui/core/Slider';
 import { withStyles, makeStyles } from '@material-ui/core/styles';
 import Paper from '@material-ui/core/Paper';
+import IconButton from '@material-ui/core/IconButton';
 import { arrayOf, object, number } from 'prop-types';
+import PlayIcon from 'mdi-react/PlayIcon';
+import PauseIcon from 'mdi-react/PauseIcon';
+import Grid from '@material-ui/core/Grid';
 
 const PrettoSlider = withStyles({
   root: {
@@ -49,39 +53,105 @@ const useStyles = makeStyles((theme) => ({
   root: {
     padding: 2 * theme.spacing.unit,
   },
+  appIcon: {
+    fill: theme.palette.text.primary,
+  },
+  slider: {
+    flexGrow: 1,
+    marginLeft: 2 * theme.spacing.unit,
+  },
 }));
 
+function useInterval(callback, delay) {
+  const intervalRef = React.useRef();
+  const callbackRef = React.useRef(callback);
+
+  // Remember the latest callback:
+  //
+  // Without this, if you change the callback, when setInterval ticks again, it
+  // will still call your old callback.
+  //
+  // If you add `callback` to useEffect's deps, it will work fine but the
+  // interval will be reset.
+
+  React.useEffect(() => {
+    callbackRef.current = callback;
+  }, [callback]);
+
+  // Set up the interval:
+
+  React.useEffect(() => {
+    if (typeof delay === 'number') {
+      intervalRef.current = window.setInterval(() => callbackRef.current(), delay);
+
+      // Clear interval if the components is unmounted or the delay changes:
+      return () => window.clearInterval(intervalRef.current);
+    }
+  }, [delay]);
+
+  // Returns a ref to the interval ID in case you want to clear it manually:
+  return intervalRef;
+}
+
+function getNetworkIndexFromEpochNumber(epochNumber, networks) {
+  const epochsLTEvalue = Object.keys(networks).filter((epoch) => (epoch <= epochNumber));
+  return epochsLTEvalue.length ? epochsLTEvalue[epochsLTEvalue.length - 1] : 0;
+}
 export default function NetworkGraph(props) {
   const classes = useStyles();
   const [networkIndex, setNetworkIndex] = useState(0);
   const [epochNumber, setEpochNumber] = useState(20);
+  const [isPlaying, setIsPlaying] = useState(false);
   const { networks, numberOfEpoch } = props;
   const onChange = useCallback((event, value) => {
     setEpochNumber(value);
     if (!networks) return;
-    const epochsLTEvalue = Object.keys(networks).filter((epoch) => (epoch <= value));
-    const epochValue = epochsLTEvalue.length ? epochsLTEvalue[epochsLTEvalue.length - 1] : 0;
-    setNetworkIndex(epochValue);
+    setNetworkIndex(getNetworkIndexFromEpochNumber(value, networks));
   }, [networks]);
 
-  if (!networks || !(networkIndex in networks)) return null;
+  const intervalRef = useInterval(() => {
+    if (epochNumber < numberOfEpoch) {
+      setEpochNumber(epochNumber + 1);
+      setNetworkIndex(getNetworkIndexFromEpochNumber(epochNumber + 1, networks));
+    } else {
+      window.clearInterval(intervalRef.current);
+    }
+  }, isPlaying ? 500 : null);
+
+  // if (!networks || !(networkIndex in networks)) return null;
   return (
     <Paper variant="outlined" classes={{ root: classes.root }}>
       <div id="treeWrapper" style={{ width: '100%', height: '80vh' }}>
-        <Tree
-          separation={{ siblings: 0.5, nonSiblings: 1.5 }}
-          data={networks[networkIndex]}
-          collapsible={false}
-          nodeSvgShape={svgSquare}
-        />
+        {(!networks || !(networkIndex in networks)) ? null
+          : (
+            <Tree
+              separation={{ siblings: 0.5, nonSiblings: 1.5 }}
+              data={networks[networkIndex]}
+              collapsible={false}
+              nodeSvgShape={svgSquare}
+            />
+          )}
       </div>
-      <PrettoSlider
-        onChangeCommitted={onChange}
-        valueLabelDisplay="auto"
-        aria-label="pretto slider"
-        value={epochNumber}
-        max={numberOfEpoch}
-      />
+      <Grid container alignItems="center">
+        <Grid item>
+          <IconButton
+            onClick={() => { setIsPlaying(!isPlaying); }}
+          >
+            {isPlaying
+              ? <PauseIcon className={classes.appIcon} size={30} />
+              : <PlayIcon className={classes.appIcon} size={30} />}
+          </IconButton>
+        </Grid>
+        <Grid item className={classes.slider}>
+          <PrettoSlider
+            onChangeCommitted={onChange}
+            valueLabelDisplay="auto"
+            aria-label="pretto slider"
+            value={epochNumber}
+            max={numberOfEpoch}
+          />
+        </Grid>
+      </Grid>
     </Paper>
   );
 }
